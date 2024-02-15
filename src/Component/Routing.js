d03a33a94c4e7e16e1a4a7d2da93689d98ef3424
satheshimman <satheshimman17@gmail.com>
2024-02-15 21:18:35 +0530

    update

--- src/Component/Routing.js
+++ src/Component/Routing.js
@@ -1,15 +1,14 @@
 import React from "react";
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./Home";
 import { Details } from "./Details";
 
-export const Routing=()=>{
-    return(
-       <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<Home/>}></Route>
-              
-          </Routes>
-       </BrowserRouter>
-    )
-}
+export const Routing = () => {
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
